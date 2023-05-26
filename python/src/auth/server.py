f32ac8ee9f1509fc943231d4af9e180c92cc2bfe
hungngo04 <ngotienhung195@gmail.com>
2023-05-26 18:05:34 +0700

    Initial commit

---
+++ python/src/auth/server.py
@@ -0,0 +1,24 @@
+import jwt, datetime, os
+from flask import Flask, request
+from flask_mysqldb import MySQL
+
+server = Flask(__name__)
+mysql = MySQL(server)
+
+# Config
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
+
+@server.route("/login", methods=["POST"])
+def login():
+    auth = request.authorization
+
+    # If the header doesn't exist
+    if not auth:
+        return "Missing credentials", 401
+    
+    # Check database for username and password
+    
