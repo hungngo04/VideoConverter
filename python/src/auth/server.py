2a1a3893675d51e0cc9bca5308aace42891129d6
Hung Ngo <htn002@bucknell.edu>
2023-09-26 20:38:39 -0400

    Auth JWT

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -20,4 +20,32 @@ def login():
         return "Missing credentials", 401
     
     # Check DB for username and password
+    cur = mysql.connection.cursor()
+    res = cur.execute(
+        "SELECT email, password FROM user WHERE email=%s", (auth.username,)
+    )
+
+    if res > 0:
+        user_row = cur.fetchone()
+        email = user_row[0]
+        password = user_row[1]
+
+        if auth.username != email or auth.password != password:
+            return "Invalid credentials", 401
+        else:
+            return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
+    else:
+        return "Invalid credentials", 401
     
+def createJWT(username, secret, authz): 
+    return jwt.encode(
+        {
+            "username": username, 
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc)
+            + datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz
+        },
+        secret,
+        algorithm="HS256"
+    )
